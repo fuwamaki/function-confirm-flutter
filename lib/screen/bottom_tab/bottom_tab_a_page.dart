3fba6ef63e9ecb6e2f076c365339ed5ff15f04ab
fuwamaki <variety.fuwa.3@gmail.com>
2022-05-11 10:29:50 +0900

    add bottoms in bottom_tab_a_page

--- lib/screen/bottom_tab/bottom_tab_a_page.dart
+++ lib/screen/bottom_tab/bottom_tab_a_page.dart
@@ -1,29 +1,58 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 
 class BottomTabAPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final localizations = AppLocalizations.of(context)!;
     return Scaffold(
       body: Scrollbar(
-        child: ListView(
-          restorationId: 'list_demo_list_view',
-          padding: const EdgeInsets.symmetric(vertical: 8),
-          children: [
-            for (int index = 1; index < 21; index++)
-              ListTile(
-                leading: ExcludeSemantics(
-                  child: CircleAvatar(child: Text('$index')),
-                ),
-                title: Text(localizations.testListItemTitle(index)),
-                subtitle: Text(localizations.testListSubText),
-                onTap: () {
-                  Fluttertoast.showToast(msg: "tap $index");
+        child: Center(
+          child: Column(
+            children: [
+              TextButton(
+                onPressed: () {
+                  Fluttertoast.showToast(msg: "tap1");
+                },
+                child: Text("Button"),
+              ),
+              const SizedBox(height: 12),
+              TextButton.icon(
+                icon: const Icon(Icons.add, size: 18),
+                label: Text("Button"),
+                onPressed: () {
+                  Fluttertoast.showToast(msg: "tap2");
                 },
               ),
-          ],
+              ElevatedButton(
+                onPressed: () {
+                  Fluttertoast.showToast(msg: "tap3");
+                },
+                child: Text("ElevatedButton"),
+              ),
+              const SizedBox(height: 12),
+              ElevatedButton.icon(
+                icon: const Icon(Icons.add, size: 18),
+                label: Text("ElevatedButton"),
+                onPressed: () {
+                  Fluttertoast.showToast(msg: "tap4");
+                },
+              ),
+              OutlinedButton(
+                onPressed: () {
+                  Fluttertoast.showToast(msg: "tap5");
+                },
+                child: Text("OutlinedButton"),
+              ),
+              const SizedBox(height: 12),
+              OutlinedButton.icon(
+                icon: const Icon(Icons.add, size: 18),
+                label: Text("OutlinedButton"),
+                onPressed: () {
+                  Fluttertoast.showToast(msg: "tap6");
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
