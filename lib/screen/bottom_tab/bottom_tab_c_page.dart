4a2cb7d20e3b5c418a7a3e093a80622884fb6c98
fuwamaki <variety.fuwa.3@gmail.com>
2022-05-24 07:46:22 +0900

    add ChoiceChip

--- lib/screen/bottom_tab/bottom_tab_c_page.dart
+++ lib/screen/bottom_tab/bottom_tab_c_page.dart
@@ -1,6 +1,27 @@
 import 'package:flutter/material.dart';
 
-class BottomTabCPage extends StatelessWidget {
+class BottomTabCPage extends StatefulWidget {
+  @override
+  _BottomTabCState createState() => _BottomTabCState();
+}
+
+class _BottomTabCState extends State<BottomTabCPage> with RestorationMixin {
+  final RestorableIntN _indexSelected = RestorableIntN(null);
+
+  @override
+  String get restorationId => 'choice_chip_demo';
+
+  @override
+  void restoreState(RestorationBucket? oldBucket, bool initialRestore) {
+    registerForRestoration(_indexSelected, 'choice_chip');
+  }
+
+  @override
+  void dispose() {
+    _indexSelected.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,6 +38,40 @@ class BottomTabCPage extends StatelessWidget {
                   color: Colors.black54,
                 ),
                 label: Text("ライトを設定"),
+              ),
+              SizedBox(height: 12),
+              Wrap(
+                children: [
+                  ChoiceChip(
+                    label: Text("small"),
+                    selected: _indexSelected.value == 0,
+                    onSelected: (value) {
+                      setState(() {
+                        _indexSelected.value = value ? 0 : -1;
+                      });
+                    },
+                  ),
+                  const SizedBox(width: 8),
+                  ChoiceChip(
+                    label: Text("medium"),
+                    selected: _indexSelected.value == 1,
+                    onSelected: (value) {
+                      setState(() {
+                        _indexSelected.value = value ? 1 : -1;
+                      });
+                    },
+                  ),
+                  const SizedBox(width: 8),
+                  ChoiceChip(
+                    label: Text("large"),
+                    selected: _indexSelected.value == 2,
+                    onSelected: (value) {
+                      setState(() {
+                        _indexSelected.value = value ? 2 : -1;
+                      });
+                    },
+                  ),
+                ],
               ),
             ],
           ),
