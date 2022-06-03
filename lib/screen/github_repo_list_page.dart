d4d678f17f7978038e0641b8702c324727645fe8
fuwamaki <variety.fuwa.3@gmail.com>
2022-06-03 14:25:28 +0900

    set StatefulWidget on github_repo_list

--- lib/screen/github_repo_list_page.dart
+++ lib/screen/github_repo_list_page.dart
@@ -1,17 +1,68 @@
 import 'package:flutter/material.dart';
 
-class GithubRepoListPage extends StatelessWidget {
+class GithubRepoListPage extends StatefulWidget {
+  const GithubRepoListPage({Key? key}) : super(key: key);
+
+  @override
+  State<GithubRepoListPage> createState() => _State();
+}
+
+class _State extends State<GithubRepoListPage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 1500),
+      vsync: this,
+      animationBehavior: AnimationBehavior.preserve,
+    )..forward();
+
+    _animation = CurvedAnimation(
+      parent: _controller,
+      curve: const Interval(0.0, 0.9, curve: Curves.fastOutSlowIn),
+      reverseCurve: Curves.fastOutSlowIn,
+    )..addStatusListener((status) {
+        if (status == AnimationStatus.dismissed) {
+          _controller.forward();
+        } else if (status == AnimationStatus.completed) {
+          _controller.reverse();
+        }
+      });
+  }
+
+  @override
+  void dispose() {
+    _controller.stop();
+    super.dispose();
+  }
+
+  Widget _buildIndicators(BuildContext context, Widget? child) {
+    return Column(
+      children: [const CircularProgressIndicator()],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text("Sample Page")),
-        body: Center(
-          child: TextButton(
-            child: Text("最初のページに戻る"),
-            onPressed: () {
-              Navigator.pop(context);
-            },
+      appBar: AppBar(
+        title: Text("Indicator"),
+      ),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Container(
+            padding: const EdgeInsets.all(8),
+            child: AnimatedBuilder(
+              animation: _animation,
+              builder: _buildIndicators,
+            ),
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
