7df020609dd9071211b0efa36c7c3a5a8f961cb1
fuwamaki <variety.fuwa.3@gmail.com>
2022-06-03 14:49:04 +0900

    rename from component to uicomponent about folder name

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'component/home_list.dart';
+import 'uicomponent/home_list.dart';
 
 void main() {
   runApp(MyApp());
