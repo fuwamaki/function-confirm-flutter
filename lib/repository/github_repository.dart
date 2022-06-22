0c42183799af359b84d756b31bf3a8e3442bfbb5
fuwamaki <variety.fuwa.3@gmail.com>
2022-06-22 09:33:00 +0900

    fix github_repository

--- lib/repository/github_repository.dart
+++ lib/repository/github_repository.dart
@@ -5,14 +5,8 @@ import 'package:http/http.dart' as http;
 class GithubRepository {
   Future<GithubResponse> fetch() async {
     try {
-      // final response = await http.get(Uri.https(
-      //     'https://api.github.com', '/search/repositories', {'q': 'dart'}));
-      var url = Uri.parse('https://api.github.com/search/repositories?q=dart');
-      final response = await http.get(url);
-      print(response.body);
-      print(response.statusCode);
-      var test = GithubResponse.fromJson(jsonDecode(response.body));
-      print(test.items.first.fullName);
+      final response = await http.get(
+          Uri.https('api.github.com', '/search/repositories', {'q': 'dart'}));
       return GithubResponse.fromJson(jsonDecode(response.body));
     } catch (error) {
       throw Exception(error.toString());
